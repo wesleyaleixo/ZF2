6e6e1cb23c2314d067117bc2815c1423e9165942
Wesley Aleixo <contato@wesleyaleixo.com>
2016-06-13 16:04:38 -0300

    Update doctrine_orm.global.php

--- config/autoload/doctrine_orm.global.php
+++ config/autoload/doctrine_orm.global.php
@@ -1,17 +1,17 @@
 <?php
 return array(
-		'doctrine' => array(
-				'connection' => array(
-						'orm_default' => array(
-								'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
-								'params' => array(
-										'host' => '',
-										'port' => '',
-										'user' => '',
-										'password' => '',
-										'dbname' => '',
-								),
-						),
+	'doctrine' => array(
+		connection' => array(
+			'orm_default' => array(
+				'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+					'params' => array(
+						'host' => '',
+						'port' => '',
+						'user' => '',
+						'password' => '',
+						'dbname' => '',
 				),
-		)			
+			),
+		),
+	)			
 );
